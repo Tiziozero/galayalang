0f898d94eb0f6bf544e563d6e9573efaa3b0531a
Tiziozero <kleidiv123@gmail.com>
2025-11-22 00:25:16 +0000

    adding strings, more tokens etc

--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 typedef enum {
     _EOF,
     IDENT,
+    O_BRAC,     // (
+    C_BRAC,     // )
+    O_CBRAC,    // {
+    C_CBRAC,    // }
+    O_SBRAC,    // [
+    C_SBRAC,    // ]
     KW,
     NUM,
     FLOAT,
@@ -25,6 +31,10 @@ typedef struct {
             size_t length;
         } ident_data;
         float f;
+        struct {
+            char* start;
+            size_t length;
+        } str_data;
     };
 } Token;
 #define current buf[i]
@@ -33,6 +43,10 @@ typedef struct {
 #define is_alpha(c) ((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
 #define is_numeric(c) ((c>='0'&&c<='9'))
 #define print_i printf("%lu\n",i)
+#define add_token(t) tokens[count++] = t
+#define empty_add_token(t) tokens[count++] = (Token){.type=t}
+#define println(msg,...) printf(msg "\n", ##__VA_ARGS__)
+#define write_buffer_of_len(buffer, len) fwrite(buffer,1,len,stdout)
 Token* lexer(char* buf, size_t length, size_t* size) {
     Token* tokens = malloc(10000*sizeof(Token));
     size_t count = 0;
@@ -94,8 +108,7 @@ Token* lexer(char* buf, size_t length, size_t* size) {
             is_numeric(current) ||
             (current == 'e' && !exponent) ||
             (current == '-' && !parsed_negative)||
-            (current == '.' && !dot_count)
-        );
+            (current == '.' && !dot_count) );
             identifier_length = (size_t)buf + i - (size_t)identifier_buffer_index;
             char* endptr;
 
@@ -133,13 +146,50 @@ Token* lexer(char* buf, size_t length, size_t* size) {
             free(tmp);
             tokens[count++] = t;
         }
-        if (peek() == '"') {
+        if (current == '"') {
+            printf("parsing string: ");
+            i++;
+            char* str_start = buf+i;
+
+            do {
+                i++;
+            } while (current!='"' || (current=='"'&&prev=='\\'));
             // parse string
-            
+            size_t len = buf+i - str_start;
+            Token t = {
+                .type=STR,
+                .str_data = {
+                    .start = str_start,
+                    .length = len,
+                }
+            };
+            // printf("\"");
+            fwrite(str_start,1,len,stdout);
+            printf("\n");
+            tokens[count++] = t;
+        }
+        if (current=='(') {
+            empty_add_token(O_BRAC);
+        }
+        if (current==')') {
+            empty_add_token(C_BRAC);
+        }
+        if (current=='[') {
+            empty_add_token(O_SBRAC);
+        }
+        if (current==']') {
+            empty_add_token(C_SBRAC);
+        }
+        if (current=='{') {
+            empty_add_token(O_CBRAC);
+        }
+        if (current=='}') {
+            empty_add_token(C_CBRAC);
         }
         i++;
     }
     printf("\n");
+    *size = count;
     return tokens;
 }
 
@@ -169,6 +219,33 @@ int main(void) {
         free(buf);
         fclose(fp);
         return -1;
+    }
+
+    for (size_t i = 0; i < token_stream_length; i++) {
+        Token t = tokens[i];
+        switch (t.type) {
+            case _EOF: println("_EOF"); break;
+            case IDENT:
+                printf("IDENT\t\t");
+                write_buffer_of_len(t.ident_data.name, t.ident_data.length);
+                println("");
+                break;
+            case O_BRAC: println("O_BRAC\t\t("); break;
+            case C_BRAC: println("C_BRAC\t\t)"); break;
+            case O_CBRAC: println("O_CBRAC\t\t{"); break;
+            case C_CBRAC: println("C_CBRAC\t\t}"); break;
+            case O_SBRAC: println("O_SBRAC\t\t["); break;
+            case C_SBRAC: println("C_SBRAC\t\ts]"); break;
+            case KW: println("KW"); break;
+            case NUM: println("NUM"); break;
+            case FLOAT: println("FLOAT"); break;
+            case CHAR: println("CHAR"); break;
+            case STR: printf("STR\t\t"); write_buffer_of_len(t.str_data.start, t.str_data.length); println(""); break;
+            case PNKT: println("PNKT"); break;
+            case PREPROC: println("PREPROC"); break;
+            case COMMENT: println("COMMENT"); break;
+            default:  println("Invalid token"); break;
+        }
     }
 
     printf("End of parsing\n");
--- test.c
+++ test.c
@@ -4,3 +4,8 @@
 static double a = 3.1415e3;
 static int b = 33e2;
 static float c = 772e-2;
+
+int main(void) {
+    char* string1 = "Hello, World!!";
+    return 0;
+}
